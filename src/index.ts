0a2be54e80fb8d69d58b6b854215c7f3c0705252
mapleincode <llkj13579@126.com>
2019-06-24 23:40:47 +0800

    feat: update api

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,5 @@ import { split } from './utils';
 export default function done(str: string, options?: object) {
     const paragraphs = split(str);
     const items = formatChapter(paragraphs);
-    for(const item of items) {
-        console.log(item.fullTitle);
-    }
+    return items;
 }
