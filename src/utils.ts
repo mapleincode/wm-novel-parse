fa685d489e7065e3e3ff65d0fe3b6f029c4f8f6c
mapleincode <llkj13579@126.com>
2019-07-01 01:59:45 +0800

    feat: suppost full-shaped

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,19 @@ export function split(str: string): string[] {
     return str.split('\n').map(str => str.trim()).filter(s => !!s);
 }
 
+const bigNums = '０１２３４５６７８９'.split('');
+const nums = '0123456789'.split('');
+
+function replaceBigNumber(num: string): string {
+    return num.split('').map(n => {
+        const index = bigNums.indexOf(n);
+        if(index > -1) {
+            return nums[index];
+        }
+        return n;
+    }).join('');
+}
+
 export function parseNumber(num: string): number {
     if(/^[0123456789]+$/.test(num)) {
         return parseInt(num);
@@ -10,6 +23,13 @@ export function parseNumber(num: string): number {
     if(/^[一二三四五六七八九十零百千万]+$/.test(num)) {
         return chinaNumberParse(num);
     }
+
+    num = replaceBigNumber(num);
+
+    if(/^[0123456789]+$/.test(num)) {
+        return parseInt(num);
+    }
+
     throw new Error(`${num} 数字非法格式`);
 }
 
